4976befbcc660a7293d3fadef5380b7e49eff9a6
Sophie Andrews <sophie.j.andrews@gmail.com>
2022-11-11 14:28:34 -0800

    brute force C++

---
+++ arith_circuits/brute_force.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+using namespace std;
+
+enum Operation {add, mult, var, constant};
+
+struct Node {
+  Operation op;
+  Node* op_a;
+  Node* op_b;
+  
+  char var_val;
+  int const_val;
+};
+
+class BruteForce {
+public:
+  vector<int> n_set;
+  int depth;
+  int prev_start;
+  vector<Node*> treesSoFar;
+  
+  BruteForce(vector<int> n_set) {
+    depth = 0;
+    treesSoFar = {};
+    treesSoFar.push_back(new Node({.op = var,
+                                   .op_a = nullptr,
+                                   .op_b = nullptr,
+                                   .var_val = 'x',
+                                   .const_val = 0}));
+    for (int i = 0; i < n_set.size(); i++) {
+      treesSoFar.push_back(new Node({.op = constant,
+                            .op_a = nullptr,
+                            .op_b = nullptr,
+                            .var_val = '\0',
+                            .const_val = i}));
+                            
+    }
+    prev_start = 0;
+  }
+
+  int grow_to(int max_depth, bool perm_lock) {
+    if (depth >= max_depth) {
+      return 0;
+    }
+    int growth = 0;
+    while (depth < max_depth) {
+      vector<Node*> newTrees = {};
+
+      for (Operation op: {add, mult}) {
+        for (int t = prev_start; t < treesSoFar.size(); t++) {
+          for (int tau = 0; tau < t; tau++) {
+              
+            newTrees.push_back(new Node{.op = op,
+                         .op_a = treesSoFar[t],
+                         .op_b = treesSoFar[tau],
+                         .var_val = '\0',
+                         .const_val = 0});
+          }
+        }
+      }
+
+      prev_start = treesSoFar.size();
+
+      treesSoFar.insert(treesSoFar.end(), newTrees.begin(), newTrees.end());
+      growth += newTrees.size();
+
+      depth += 1;
+    }
+    return growth;
+  }
+}; 
+
+bool treeEqual(Node* a, Node* b) {
+  if (!a && !b) {
+    return true;
+  }
+  if (!a || !b) {
+    return false;
+  }
+  return a->op == b -> op && a->var_val == b->var_val && a->const_val == b->const_val &&  treeEqual(a->op_a, b->op_a) && treeEqual(a->op_b, b->op_b);
+}
+
+int main() {
+  BruteForce bf = BruteForce({1, 2});
+
+  for (int i = 0; i < 4; i++) {
+    bf.grow_to(i, false);
+    cout << i << endl;
+    cout << bf.treesSoFar.size() << endl;
+  }
+  
+  return 0;
+}
+
+
