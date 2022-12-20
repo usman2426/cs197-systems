a6eefffafb7e7a985ff3d52397649bfbb5afadbe
tweoss <francischua689@gmail.com>
2022-12-19 16:25:33 -0800

    rewrote bit to be simpler and faster

--- inclusion_exclusion/prototype_solver/src/clauses/bit.rs
+++ inclusion_exclusion/prototype_solver/src/clauses/bit.rs
@@ -13,21 +13,20 @@ pub struct BitClause {
 }
 
 impl Merge for BitClause {
-    fn merge(a: Self, b: &Self, _total_size_hint: u32) -> MergeResult<Self> {
-        let mut conflicts = a.signs.clone();
-        conflicts.symmetric_difference_with(&b.signs);
-        conflicts.intersect_with(&a.literals);
-        conflicts.intersect_with(&b.literals);
-        // same logic as in compressed bitset
-        if !conflicts.is_empty() {
-            MergeResult::Incompatible
-        } else {
-            // returns L1 | L2, S1 | S2
-            MergeResult::Set(BitClause {
-                literals: a.literals.union(&b.literals).collect(),
-                signs: a.signs.union(&b.signs).collect(),
-            })
+    fn merge(mut a: Self, b: &Self, _total_size_hint: u32) -> MergeResult<Self> {
+        for l in b.literals.iter() {
+            if !a.literals.insert(l) {
+                if a.signs.contains(l) != b.signs.contains(l) {
+                    return MergeResult::Incompatible;
+                }
+            } else {
+                // only for positive
+                if b.signs.contains(l) {
+                    a.signs.insert(l);
+                }
+            }
         }
+        MergeResult::Set(a)
     }
 
     fn from_vec(vec: Vec<Vec<(u32, Sign)>>) -> DNF<Self> {
